4f6553149361ec4fb9ff6ac38b2eb2b058a8bd2f
John Lonergan <john.lonergan@gmail.com>
2017-03-07 00:43:31 +0000

    upgraded classpathHell

--- src/main/java/slf4jtest/LoggerFactoryExtensions.java
+++ src/main/java/slf4jtest/LoggerFactoryExtensions.java
@@ -4,13 +4,22 @@ import org.slf4j.ILoggerFactory;
 
 public interface LoggerFactoryExtensions extends ILoggerFactory {
 
-    /** get or create the given logger  */
+    /** get or create the given logger.
+     * @param  name the SLF4J logger name.
+     * @return the TestLogger
+     */
     public TestLogger getLogger(Class<?> name);
 
-    /** check but doesn't create the logger */
+    /** check but doesn't create the logger
+     * @param  name the SLF4J logger name.
+     * @return the TestLogger
+    */
     public boolean loggerExists(String name);
 
-    /** check but doesn't create the logger */
+    /** check but doesn't create the logger
+     * @param  name the SLF4J logger name.
+     * @return the TestLogger.
+     */
     public boolean loggerExists(Class<?> name);
 
 }
